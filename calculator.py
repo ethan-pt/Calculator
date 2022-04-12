037a6d25f96c9a221d40b77c9afa711fc57ac693
Assentt <tubbeethan@gmail.com>
2022-04-12 16:23:53 -0700

    Display button widgets

--- calculator.py
+++ calculator.py
@@ -34,6 +34,29 @@ button_add = Button(root, text="+", padx=29, pady=52, bd=1, command=add)
 button_equals = Button(root, text="=", padx=29, pady=52, bd=1, command=equals)
 button_decimal = Button(root, text=".", padx=31, pady=20, bd=1, command=lambda: num('.'))
 
+# display buttons
+button_clear.grid(row=1, column=0)
+button_divide.grid(row=1, column=1)
+button_multiply.grid(row=1, column=2)
+button_subtract.grid(row=1, column=3)
+
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
+button_add.grid(row=2, column=3, rowspan=2)
+
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
+
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
+button_equals.grid(row=4, column=3, rowspan=2)
+
+button_0.grid(row=5, column=0, columnspan=2)
+button_decimal.grid(row=5, column=2)
+
 
 # window loop
 root.mainloop()
