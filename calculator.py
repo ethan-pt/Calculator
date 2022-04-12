83f88119cf349bae513374f0cbd776a94273250a
Assentt <tubbeethan@gmail.com>
2022-04-12 16:19:04 -0700

    Create entry widget

--- calculator.py
+++ calculator.py
@@ -8,5 +8,12 @@ root = Tk()
 root.title("Calculator")
 
 
+# define entry
+entry = Entry(root, width=19, font='Arial 20', bd=0)
+
+# display entry
+entry.grid(row=0, column=0, columnspan=4)
+
+
 # window loop
 root.mainloop()
